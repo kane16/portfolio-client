584060812ea5c426f8b6b043768a3b7a450b235f
Łukasz Gumiński <lukasz.guminski629@gmail.com>
2025-07-20 09:35:30 +0200

    Ddd React Query DevTools, and ensure QueryClient is globally accessible

--- src/main.tsx
+++ src/main.tsx
@@ -6,6 +6,15 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query"
 
 const queryClient = new QueryClient()
 
+declare global {
+  interface Window {
+    __TANSTACK_QUERY_CLIENT__: import("@tanstack/query-core").QueryClient
+  }
+}
+
+// This code is for all users
+window.__TANSTACK_QUERY_CLIENT__ = queryClient
+
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <QueryClientProvider client={queryClient}>
