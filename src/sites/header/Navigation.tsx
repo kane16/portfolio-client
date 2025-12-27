80e2ec0b99bdb818adf961f36978329b559d23bc
Łukasz Gumiński <lukasz.guminski629@gmail.com>
2025-12-27 10:35:06 +0100

    fix: Correct blog link path in Navigation component

--- src/sites/header/Navigation.tsx
+++ src/sites/header/Navigation.tsx
@@ -7,7 +7,7 @@ export default function Navigation(): JSX.Element {
   return (
     <div className="flex w-full flex-col items-center sm:flex-row sm:justify-center sm:gap-6">
       <NavLink title={t("nav.cv")} href="/api/portfolio/pdf" newTab={true} />
-      <NavLink title={t("nav.blog")} href="/blog" newTab={true} />
+      <NavLink title={t("nav.blog")} href="/blog/" newTab={true} />
     </div>
   )
 }
